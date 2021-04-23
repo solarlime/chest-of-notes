60b1e1dc211be0b55c29a2d62eac67867a5f641f
solarlime <solarlime@outlook.com>
2021-04-23 23:24:23 +0300

    Added screens & some styles for them (with resizing)

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,17 @@
 export default class App {
   static init() {
+    /**
+     * A listener for resizing. Works good for mobiles
+     */
+    window.addEventListener('resize', () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+    window.dispatchEvent(new Event('resize'));
+
+    const main = document.querySelector('.main');
+    main.scrollIntoView();
+
     console.log('init');
   }
 }
