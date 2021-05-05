a148e7b437bd3062ff1cadb2523205b52e95129d
solarlime <solarlime@outlook.com>
2021-05-06 01:08:49 +0300

    Added functionality for 'add' & 'back' buttons

--- src/js/page.js
+++ src/js/page.js
@@ -1,7 +1,10 @@
 export default class Page {
   constructor() {
     this.page = document.body;
+    this.add = this.page.querySelector('.add');
     this.notes = this.page.querySelector('.notes');
+    this.addButton = this.page.querySelector('button.notes-go-to-add-button');
+    this.backButton = this.page.querySelector('button.back-button');
     this.footerLogo = this.page.querySelector('.footer-logo');
     this.about = this.page.querySelector('.about');
 
@@ -10,13 +13,23 @@ export default class Page {
 
   addEventListeners() {
     /**
-         * A function for showing an 'about' modal;
-         */
+     * A listener for showing an 'about' modal
+     */
     this.footerLogo.addEventListener('click', () => this.about.classList.add('active'));
 
     /**
-         * A listener for hiding an 'about' modal after the animation end;
-         */
+     * A listener for hiding an 'about' modal after the animation end
+     */
     this.about.addEventListener('animationend', () => this.about.classList.remove('active'));
+
+    /**
+     * A listener for an 'add' button
+     */
+    this.addButton.addEventListener('click', () => this.add.scrollIntoView({ behavior: 'smooth' }));
+
+    /**
+     * A listener for a 'back' button
+     */
+    this.backButton.addEventListener('click', () => this.notes.scrollIntoView({ behavior: 'smooth' }));
   }
 }
