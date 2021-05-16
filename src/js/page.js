d7b09e883b744e11d73fc37a0f5cd9ee642826ad
solarlime <solarlime@outlook.com>
2021-05-16 08:48:13 +0300

    Added some logic for a modal

--- src/js/page.js
+++ src/js/page.js
@@ -5,6 +5,11 @@ export default class Page {
     this.notes = this.page.querySelector('.notes');
     this.addButton = this.page.querySelector('button.notes-go-to-add-button');
     this.backButton = this.page.querySelector('button.back-button');
+    this.audioButton = this.page.querySelector('button.audio-button');
+    this.videoButton = this.page.querySelector('button.video-button');
+    this.textButton = this.page.querySelector('button.text-button');
+    this.modalAdd = this.page.querySelector('.modal-add');
+    this.modalCloseButton = this.page.querySelector('button.close');
     this.footerLogo = this.page.querySelector('.footer-logo');
     this.about = this.page.querySelector('.about');
 
@@ -31,5 +36,17 @@ export default class Page {
      * A listener for a 'back' button
      */
     this.backButton.addEventListener('click', () => this.notes.scrollIntoView({ behavior: 'smooth' }));
+
+    /**
+     * Listeners to open the content modal
+     */
+    [this.audioButton, this.videoButton, this.textButton].forEach((button) => button.addEventListener('click', (event) => {
+      console.log(event);
+      this.page.querySelectorAll('section, footer').forEach((item) => item.classList.add('blur'));
+      this.modalAdd.classList.add('modal-active');
+    }));
+
+    this.modalCloseButton.addEventListener();
+
   }
 }
