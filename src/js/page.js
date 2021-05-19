ef9b033ebc86f7a34ea0d27a308c6cc0c486ddaa
solarlime <solarlime@outlook.com>
2021-05-19 10:30:35 +0300

    Now the modal shows only the necessary fields

--- src/js/page.js
+++ src/js/page.js
@@ -12,6 +12,11 @@ export default class Page {
     this.textButton = this.page.querySelector('button.text-button');
     this.background = this.page.querySelectorAll('section, footer');
     this.modalAdd = this.page.querySelector('.modal-add');
+    this.modalTextForm = this.page.querySelector('.modal-add-form-text');
+    this.modalFormDescriptionMedia = this.page.querySelector('.modal-add-form-description-media');
+    this.modalStartButton = this.page.querySelector('button.start');
+    this.modalStopButton = this.page.querySelector('button.stop');
+    this.modalSaveButton = this.page.querySelector('button.save');
     this.modalCloseButton = this.page.querySelector('button.close');
     this.footerLogo = this.page.querySelector('.footer-logo');
     this.about = this.page.querySelector('.about');
@@ -51,6 +56,25 @@ export default class Page {
      */
     [this.audioButton, this.videoButton, this.textButton].forEach((button) => button.addEventListener('click', (event) => {
       event.preventDefault();
+
+      switch (button) {
+        case this.audioButton: {
+          this.modalTextForm.classList.add('hidden');
+          break;
+        }
+        case this.videoButton: {
+          this.modalTextForm.classList.add('hidden');
+          break;
+        }
+        case this.textButton: {
+          [this.modalStartButton, this.modalStopButton, this.modalFormDescriptionMedia].forEach((item) => item.classList.add('hidden'));
+          break;
+        }
+        default: {
+          console.log('Hmm, something else happened!');
+        }
+      }
+
       const notNew = Array.from(this.background).find((item) => item.classList.contains('remove-blur'));
       if (notNew) {
         this.background.forEach((item) => item.classList.toggle('blur'));
@@ -63,12 +87,20 @@ export default class Page {
       }
     }));
 
+    /**
+     * Listeners to close the content modal
+     */
     this.modalCloseButton.addEventListener('click', (event) => {
       event.preventDefault();
       this.background.forEach((item) => item.classList.toggle('blur'));
       this.background.forEach((item) => item.classList.toggle('remove-blur'));
       this.modalAdd.classList.toggle('modal-active');
       this.modalAdd.classList.toggle('modal-inactive');
+      setTimeout(() => {
+        // eslint-disable-next-line max-len
+        [this.modalTextForm, this.modalStartButton, this.modalStopButton, this.modalFormDescriptionMedia]
+          .forEach((item) => (item.classList.contains('hidden') ? item.classList.remove('hidden') : null));
+      }, 1000);
     });
   }
 }
