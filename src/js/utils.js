204569794dcbe3cbbae250a2ad3951debe80a657
solarlime <solarlime@outlook.com>
2021-08-07 10:24:14 +0300

    Bugfix: new notes didn't render correctly

--- src/js/utils.js
+++ src/js/utils.js
@@ -98,13 +98,13 @@ export async function recordSomeMedia(media) {
 }
 
 export function renderNewNote(notesList, data) {
-  const isText = (data.type === 'text') ? data.content : null;
+  const isText = (data.type === 'text') ? data.content : 'media';
   const notesListItem = document.createElement('li');
   notesListItem.classList.add('notes-list-item');
   notesListItem.innerHTML = `<input type="checkbox" id="${data.id}" class="checkbox">\n`
     + '                    <div class="notes-list-item-header-wrapper">\n'
     + `                    <h3 class="notes-list-item-header">${data.name}</h3>\n`
-    + `                        <label class="spoiler" for="${data.id}">\n`
+    + `                        <label class="spoiler ${(!data.content) ? 'hidden' : ''}" for="${data.id}">\n`
     + '                            <svg class="spoiler-svg" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 490.656 490.656" xml:space="preserve">\n'
     + '                                <g><g>\n'
     + '                                    <path d="M487.536,120.445c-4.16-4.16-10.923-4.16-15.083,0L245.339,347.581L18.203,120.467c-4.16-4.16-10.923-4.16-15.083,0    c-4.16,4.16-4.16,10.923,0,15.083l234.667,234.667c2.091,2.069,4.821,3.115,7.552,3.115s5.461-1.045,7.531-3.136l234.667-234.667    C491.696,131.368,491.696,124.605,487.536,120.445z"/>\n'
@@ -112,7 +112,7 @@ export function renderNewNote(notesList, data) {
     + '                            </svg>\n'
     + '                        </label>\n'
     + '                    </div>\n'
-    + `                    <p class="notes-list-item-description">${(isText) || 'Click to open the media!'}</p>`;
+    + `                    <p class="notes-list-item-description ${(!data.content) ? 'hidden' : ''}">${(isText !== 'media') ? isText : 'Click to open the media!'}</p>`;
   notesList.append(notesListItem);
   return { notesListItem, isText };
 }
