3303f046e03082a257ea1a8781ec0754111b9e3d
solarlime <solarlime@outlook.com>
2023-07-19 00:13:26 +0300

    [Backport] Added a simple confirmation for reloading the page on WebSocket disconnection

--- src/js/page.js
+++ src/js/page.js
@@ -54,6 +54,11 @@ export default class Page {
             const timeout = setTimeout(() => { clearTimeout(timeout); alert(text); }, 500);
           } else {
             console.log('Subscribed on notifications!');
+            client.addEventListener('close', () => {
+              console.log('Connection was closed!');
+              const confirmation = window.confirm('A server connection was lost. Do you want reload the page and try to connect again?');
+              if (confirmation) window.location.reload();
+            });
           }
         }
         if (data.event) {
